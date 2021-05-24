7c3c42fd7f17e5aea77d053dfd92bb226f8ae3d4
listout <listout@protonmail.com>
2021-05-24 21:34:40 +0530

    changed a few parameters in imagedatagenerator

--- prediction/pred.py
+++ prediction/pred.py
@@ -22,15 +22,11 @@ model = tf.keras.models.load_model(
     options=None)
 
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
+    rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    brightness_range=(.3, 1.),
     horizontal_flip=True,
     vertical_flip=True,
-    fill_mode='nearest',
     preprocessing_function=preprocess_input,
     validation_split=0.2
 )
